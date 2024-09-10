c5e7d067daa864c78621f814280c4306b9f4cf9b
JoaquinDalmasso <joacodalmasso@gmail.com>
2024-09-10 20:15:02 -0300

    funcionando

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,30 +1,45 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, Pressable } from 'react-native'
+import { Link } from 'expo-router'
 
-export default function ExerciseListItem({ item }) {
+export default function ExerciseListItem ({ item }){
     return(
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>
-            {item.name}
-        </Text>
-        <Text style={styles.exerciseSubtitle}>
-            {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
-        </Text>
-      </View>
+        <Link href={`/${item.name}`} asChild>
+            <Pressable style={styles.exerciseContainer}>
+            <Text  style={styles.exerciseName}>{item.name}</Text>
+                <Text style={styles.exerciseSubtitle}>
+                <Text style={styles.subValue}>{item.muscle}</Text> |{' '} <Text style={styles.subValue}>{item.equipment}</Text>
+                </Text>
+            </Pressable>
+        </Link>
     )
-  }
+}
 
 const styles = StyleSheet.create({
-    exerciseContainer:{
+    exerciseContainer: {
         backgroundColor: '#fff',
         padding: 10,
         borderRadius: 10,
         gap: 5,
+        marginHorizontal: 2,
+
+        //shadow
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
     },
-    exerciseName:{
+    exerciseName: {
         fontSize: 20,
         fontWeight: '500',
     },
-    exerciseSubtitle:{
+    exerciseSubtitle: {
         color: 'dimgray',
     },
+    subValue: {
+        textTransform: 'capitalize',
+    }
 })
